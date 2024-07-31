ce9ccf3cf0678e277938f73eac85a2f7f6ddc7ec
Yofou <reach@yofou.dev>
2024-08-01 00:43:47 +0100

    chore: named context providers

--- src/context.ts
+++ src/context.ts
@@ -2,11 +2,13 @@ import { createHeadManager, Page } from "@inertiajs/core";
 import { createContext, useContext } from "kaioken";
 
 export const PageContext = createContext<Page | null>(null)
+PageContext.displayName = 'InertiaPageContext'
 export const usePage = () => {
   return useContext(PageContext)!
 }
 
 export const HeadContext = createContext<ReturnType<typeof createHeadManager> | null>(null)
+HeadContext.displayName = 'InertiaHeadContext'
 export const useHead = () => {
   return useContext(HeadContext)!
 }
